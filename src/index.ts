9cab664860b970cfac1b4c2a0d0db9e6d5ba0a00
Luan Diniz Barreiros <luanminasgerais@hotmail.com>
2024-11-28 17:53:07 -0300

    adiconando cors

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { askGemini } from "./routes/geminiRoute";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors<Request>());
 app.use("/", askGemini);
 app.get("/teste", (req: Request, res: Response) => {
   res.send("teste");
