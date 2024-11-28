63257b6ac4a630352254b63b10774e96d4a5ce27
Luan Barreiros <luan_barreiros@trimble.com>
2024-11-28 15:55:49 -0300

    removing text for tests

--- src/services/geminiService.ts
+++ src/services/geminiService.ts
@@ -2,14 +2,14 @@ import geminiRepository from "../repositories/geminiRepository";
 
 const callGemini = async (input: string) => {
   // TODO Implement your content generation logic here
-
-  const premadeText = `todas as respostas precisam estar relacionadas a receitas, priorizando 
-  as brasileiras tradicionais (como feijoada, pão de queijo, brigadeiro, etc.) e receitas internacionais 
-  amplamente conhecidas (como lasanha, sushi, pizza, etc.). As respostas devem ser apresentadas exclusivamente
-   no formato JSON, com as propriedades: "nome" (nome da receita), "ingredientes" (lista detalhada de ingredientes),
-    "tempo_de_preparo" (tempo estimado de preparo em minutos ou horas), "dificuldade_de_preparo" (nível de dificuldade: fácil, médio ou difícil) 
-    e "preparo" (passo a passo detalhado para executar a receita). Além disso, as respostas devem ser limitadas 
-    a 350 palavras e não podem incluir informações fora do contexto de receitas ou culinária.`;
+  const premadeText = "";
+  // const premadeText = `todas as respostas precisam estar relacionadas a receitas, priorizando
+  // as brasileiras tradicionais (como feijoada, pão de queijo, brigadeiro, etc.) e receitas internacionais
+  // amplamente conhecidas (como lasanha, sushi, pizza, etc.). As respostas devem ser apresentadas exclusivamente
+  //  no formato JSON, com as propriedades: "nome" (nome da receita), "ingredientes" (lista detalhada de ingredientes),
+  //   "tempo_de_preparo" (tempo estimado de preparo em minutos ou horas), "dificuldade_de_preparo" (nível de dificuldade: fácil, médio ou difícil)
+  //   e "preparo" (passo a passo detalhado para executar a receita). Além disso, as respostas devem ser limitadas
+  //   a 350 palavras e não podem incluir informações fora do contexto de receitas ou culinária.`;
   input = input + premadeText;
   return await geminiRepository.generateContent(input);
 };
